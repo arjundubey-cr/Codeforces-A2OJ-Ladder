43517673f4adc96a1c67008df57be2fab84abb58
arjundubey-cr <arjundubeycr@gmail.com>
2020-06-10 16:10:33 +0530

    commitfile

--- Codeforces Rating < 1300/09. Nearly Lucky Number.cpp
+++ Codeforces Rating < 1300/09. Nearly Lucky Number.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 using namespace std;
-void solve()
+
+int solve()
 {
+	string s;
+	cin>>s;
+	int size=s.size();
+	for(int i=0;i<size;i++)
+	{
+		if(s[i]!='4'||s[i]!='7')
+		{
+			return 0;
+		}
+	}
+	return 1;
 	
 }
+
 int main()
 {
-	
+	int val = solve();
+	if(!val)
+	{
+		cout<<"NO\n";
+	}
+	else{
+		cout<<"YES\n";
+	}
 }
