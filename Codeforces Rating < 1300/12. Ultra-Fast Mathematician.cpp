a54b3b282f0fc275ce502ed41a1501c2ee381ab0
arjundubey-cr <arjundubeycr@gmail.com>
2020-06-15 00:07:49 +0530

    addsolutions

--- Codeforces Rating < 1300/12. Ultra-Fast Mathematician.cpp
+++ Codeforces Rating < 1300/12. Ultra-Fast Mathematician.cpp
@@ -1,13 +1,23 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-void solve()
-{
-	
-}
-
 int main()
 {
-	
+    string s1, s2;
+    cin >> s1 >> s2;
+    for (size_t i = 0; i < s1.length(); ++i)
+    {
+        if (s1[i] == s2[i])
+        {
+            s1[i] = '0';
+        }
+        else
+        {
+            s1[i] = '1';
+        }
+    }
+    cout << s1 << endl;
+    return 0;
 }
