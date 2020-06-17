90de2557613e3f26e55162fd6b5d254bb3cf6ed8
arjundubey-cr <arjundubeycr@gmail.com>
2020-06-18 01:48:51 +0530

    commitchanges

--- Codeforces Rating < 1300/17. Cupboards.cpp
+++ Codeforces Rating < 1300/17. Cupboards.cpp
@@ -4,10 +4,21 @@ using namespace std;
 
 void solve()
 {
-	
+	string b;
+	cin>>b;
+	string a=b.substr(0,2);
+	int len=b.size();
+	for(int i=3; i<len; i+=2)
+	{
+		a+=b[i];
+	}
+	cout<<a<<endl;
 }
 
 int main()
 {
-	
+	int t;
+	cin>>t;
+	while(--t)
+		solve();
 }
