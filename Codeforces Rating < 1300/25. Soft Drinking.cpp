45d0dc05d40ce7cd7a192117de1d002b4b732d55
arjundubey-cr <arjundubeycr@gmail.com>
2020-06-23 18:35:48 +0530

    commitcode

--- Codeforces Rating < 1300/25. Soft Drinking.cpp
+++ Codeforces Rating < 1300/25. Soft Drinking.cpp
@@ -1,25 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-void solve()
-{
-	int n,k,l,c,d,p,nl,np;
-	cin>>n>>k>>l>>c>>d>>p>>nl>>np;
-	//int totalSlice=c*d;
-	int totalDrink=k*l;
-	//int counter=0;
-	
-	int drink=totalDrink/n;
-	int limes=c*d;
-	int salt=p/np;
-	
-	int toastValue=min(min(drink, limes), min(drink, salt));
-	
-	cout<<toastValue/n;
-}
-
 int main()
 {
-	solve();
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+    cout << min(min(k * l / nl, c * d), p / np) / n << endl;
+    return 0;
 }
